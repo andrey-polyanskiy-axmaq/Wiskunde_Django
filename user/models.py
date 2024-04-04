812c9391e12229ab773741126aa5fae58098cd7e
r0mch1ck <roma.kovalev2005@mail.ru>
2024-04-04 21:38:39 +0300

    -

--- user/models.py
+++ user/models.py
@@ -1,13 +1,14 @@
-from django.contrib.auth.models import AbstractUser
+'''from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
-    group = models.IntegerField()
-    login_field = models.CharField(max_length=50)
+    # group = models.IntegerField()  # Предполагается, что это должно быть ForeignKey, если это ссылка на другую модель
+    # login_field = models.CharField(max_length=50)  # Необходимо уточнить назначение этого поля
     course_bought = models.BooleanField(default=True)
     date_of_next_payment = models.DateField()
-    is_active = models.BooleanField(default=True)
+    # is_active = models.BooleanField(default=True)  # Это поле уже определено в AbstractUser, его переопределение не требуется
 
     def __str__(self):
         return self.username
+'''
--- wiskunde/settings.py
+++ wiskunde/settings.py
@@ -133,4 +133,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # или 'django.contrib.sessions.backends.cache', в зависимости от ваших настроек
 
-AUTH_USER_MODEL = 'user.CustomUser'
+'''AUTH_USER_MODEL = 'user.CustomUser'
+'''
