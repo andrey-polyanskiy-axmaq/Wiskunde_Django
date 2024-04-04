808f5ca1cca437f352beb5768712fe095b127abf
r0mch1ck <roma.kovalev2005@mail.ru>
2024-04-04 20:58:26 +0300

    Upd: models.py

--- user/views.py
+++ user/views.py
@@ -2,40 +2,40 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm
 
-def logout_user(request):
+def logout_CustomUser(request):
     logout(request)
     return redirect('home')
 
 def index(request):
     context = {
-        'username': request.user.username,
-        'first_name': request.user.first_name,
-        'last_name': request.user.last_name,
-        'email': request.user.email,
-        'date_joined': request.user.date_joined,
-        'last_login': request.user.last_login,
+        'username': request.CustomUser.CustomUsername,
+        'first_name': request.CustomUser.first_name,
+        'last_name': request.CustomUser.last_name,
+        'email': request.CustomUser.email,
+        'date_joined': request.CustomUser.date_joined,
+        'last_login': request.CustomUser.last_login,
     }
-    return render(request, 'user/index.html', context)
+    return render(request, 'CustomUser/index.html', context)
 
-def user_login(request):
-    if request.user.is_authenticated:
-        return redirect('userinfo')
+def CustomUser_login(request):
+    if request.CustomUser.is_authenticated:
+        return redirect('CustomUserinfo')
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    if request.user.is_superuser:
+            CustomUser = authenticate(CustomUsername=cd['CustomUsername'], password=cd['password'])
+            if CustomUser is not None:
+                if CustomUser.is_active:
+                    login(request, CustomUser)
+                    if request.CustomUser.is_superCustomUser:
                         return redirect('/admin/')
                     else:
-                        return redirect('userinfo')
+                        return redirect('CustomUserinfo')
                 else:
-                    return render(request, 'user/loginWPS.html', {'form': form})
+                    return render(request, 'CustomUser/loginWPS.html', {'form': form})
             else:
-                return render(request, 'user/loginWPS.html', {'form': form})
+                return render(request, 'CustomUser/loginWPS.html', {'form': form})
     else:
         form = LoginForm()
-    return render(request, 'user/login.html', {'form': form})
+    return render(request, 'CustomUser/login.html', {'form': form})
