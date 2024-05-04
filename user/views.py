2e46dec7cdfe852c9bad5e153a61435c586d0392
r0mch1ck <roma.kovalev2005@mail.ru>
2024-05-05 02:23:41 +0300

    -

--- user/views.py
+++ user/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4636c10e7feee07f5a04a3d104aab63617ce673d0cff2f06cad1379a28538acb
-size 14286
+oid sha256:a37c08ac42ea9d24685ffcc35e6b11bc585001cc5ea0cfc2e07457979eef0175
+size 13903
