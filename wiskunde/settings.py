c194a4387a47acd4b64d5c26d095f9ff6be25e92
r0mch1ck <roma.kovalev2005@mail.ru>
2024-05-11 17:25:43 +0300

    -

--- wiskunde/settings.py
+++ wiskunde/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e04709dec516425da0baab7a8a6c8a6aa46f6724f7088cb8a1506fdd143e276
+oid sha256:0a20f00d6b1ac5448e4c50c117bd15b77f44b0e5c016f9c40331db7be71939fb
 size 5377
