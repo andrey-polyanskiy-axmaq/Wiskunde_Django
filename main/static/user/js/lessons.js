0e576d449c11bb8ddaff1c69c43fad3445c49d82
komarova-olya-vladimirovna <komarovaov@student.bmstu.ru>
2024-05-23 00:34:35 +0300

    refactoring

--- main/static/user/js/lessons.js
+++ main/static/user/js/lessons.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afd3b894f46314dfcd8b2b2e32d0c9dacd8b081c635065114e145a61d2610d97
-size 1138
+oid sha256:44d17b44378f0f09db859036b1f5ea72e7e67c63d66c840158e327ae0a1073bc
+size 1481
