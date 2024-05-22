da2e63eeffc61237357d2014481a341397f817e7
r0mch1ck <roma.kovalev2005@mail.ru>
2024-05-22 18:41:36 +0300

    -

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdbe1f365eac5bf7499ca4c2befe86593f785a7e4985fbd0c2e230eccbabd270
-size 3227
+oid sha256:59b60dc692b599519973400b70398dc75f82bdeea8af490913a052ad976b3990
+size 3291
---
+++ user/migrations/0003_customuser_summer_course_customuser_year_course.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a408788ea0e9bee2bf64c21925bcd99ff0ef882e36cf70af7af8684878c3961
+size 623
