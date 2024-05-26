7ae21f12dfc9eef27c0c7a6c2e8bf825567cfa5f
komarova-olya-vladimirovna <komarovaov@student.bmstu.ru>
2024-05-27 00:38:57 +0300

    refactoring

--- user/forms.py
+++ user/forms.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:217b81db3b49f8f3be7affd4adcb9085049036c73834f13b0e4ac005d155ad07
-size 1513
+oid sha256:3eaf22100f514bb39384d80eae8b4b0301791c0504aaacc3445eb4d9f9dc4e80
+size 1564
